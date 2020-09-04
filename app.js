e37c0ba09f7ec81f050876731d6e2ed532c84c3b
Nnaemeka <N.nnaemeka@gmail.com>
2020-09-04 07:44:30 -0500

    added JS counter code

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
- 
+const counterSpeed = 200;
+
+document.querySelectorAll('#statCouter').forEach(statCouter => {
+    const counterNum = +statCouter.getAttribute('countTo');
+
+    const update = setInterval(()=> {
+        const contentUpdate = +statCouter.innerText;
+        const increase = counterNum / counterSpeed;
+
+        contentUpdate < counterNum ?
+        statCouter.innerHTML = Math.ceil(contentUpdate + increase) :
+       
+        clearInterval(contentUpdate);
+
+    }, 1);
+});
